1089fd31ea9b49db996beddf0ceffdbef4a4de54
Matias <matiasenriquegarciadev@gmail.com>
2024-02-29 00:41:47 -0300

    Update slider

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,19 @@
 
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Glider from './glider/Glider'
+
 
 function App() {
   
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
+      
       <div className="card">
-        
+        <Glider/>
       </div>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+        Best game in on the market 2024
       </p>
     </>
   )
--- src/glider/Glider.tsx
+++ src/glider/Glider.tsx
@@ -1,9 +1,60 @@
 import React from 'react'
+import {Swiper, SwiperSlide} from "swiper/react"
+import 'swiper/css'
+import 'swiper/css/effect-coverflow'
+import 'swiper/css/pagination'
+import 'swiper/css/navigation'
 
-const gliderJs = () => {
+import { EffectCoverflow, Pagination, Navigation} from 'swiper'
+
+import ac_image from "../assets/AC.webp"
+import BG3_image from "../assets/BG3.jpg"
+import inmortal_image from "../assets/inmortal.webp"
+import mario_image from "../assets/Mario.webp"
+import './Glider.css'
+
+
+const Glider = () => {
+
   return (
-    <div>gliderJs</div>
+
+    <div className="glide">
+      <h1> Game Gallery </h1>
+      <Swiper
+        effect={ 'coverflow' }
+        grabCursor= {true}
+        centeredSlides= {true}
+        loop={true}
+        coverflowEffect={
+          {
+            rotate: 0,
+            stretch: 0,
+            depth: 100,
+            modifier: 2.5,
+          }
+        }
+        className='swiper-container'
+      >
+        <SwiperSlide>
+          <img src={ac_image} alt="assasine creed" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={BG3_image} alt="BG3" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={inmortal_image} alt="inmortal" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={mario_image} alt="Mario" />
+        </SwiperSlide>
+
+        <div className="slider_controler">
+          
+        </div>
+
+      </Swiper>
+    </div>
   )
 }
 
-export default gliderJs
+export default Glider
